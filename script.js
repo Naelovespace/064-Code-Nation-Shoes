2b490ac1a62d126ffc0a02f43b21c484118ccdcf
Naelovespace <106091059+Naelovespace@users.noreply.github.com>
2022-05-23 09:10:06 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".first").click(function() {
+    $(".mainImage").css("background-color", "grey");
+});
+
+$(".second").click(function() {
+    $(".imageCarousel").css("background-color", "orange");
+});
+
+$(".thrid").click(function() {
+    $(".imageHolder").css("background-color", "lightgreen");
+});
+
+$(".fourth").click(function() {
+    $(".product").css("background-color", "cyan");
+});
